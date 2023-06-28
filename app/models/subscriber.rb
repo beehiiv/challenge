5980a1e57e41ad5ab0efd0086aae206a26c6c0cf
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-06-28 10:08:51 -0600

    Initial model validations/tests.

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,2 +1,5 @@
 class Subscriber < ApplicationRecord
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :status, presence: true
 end
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Subscriber, :type => :model do
     expect(Subscriber.new(email: 'marys@email.com', name: 'Mary Smith', status: 'active')).to be_valid
     expect(Subscriber.new(email: 'maryj@email.com', name: 'Mary Jones', status: 'inactive' )).to be_valid
   end
+
+    it "is not valid without an email" do
+    subscriber = Subscriber.new(email: nil)
+    expect(subscriber).to_not be_valid
+  end
+
+  it "is not valid without a name" do
+    subscriber = Subscriber.new(name: nil)
+    expect(subscriber).to_not be_valid
+  end
+
+  it "is not valid without a status" do
+    subscriber = Subscriber.new(status: nil)
+    expect(subscriber).to_not be_valid
+  end
 end
