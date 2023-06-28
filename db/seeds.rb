3b84118bfaf3ca213523f53b5c74bf0cbedcaed3
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-06-28 11:04:45 -0600

    Adds faker gem and seeds for database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# original demo subscribers from subscribers controller
+subscribers = [
+  {
+    name: "Rick Sanchez",
+    email: "rickc137@citadel.com",
+    status: "active"
+  },
+  {
+    name: "Morty Smith",
+    email: "morty.smith@gmail.com",
+    status: "inactive"
+  },
+  {
+    name: "Jerry Smith",
+    email: "jerry.smith@aol.com",
+    status: "active"
+  },
+  {
+    name: "Beth Smith",
+    email: "beth.smith@gmail.com",
+    status: "active"
+  },
+  {
+    name: "Summer Smith",
+    email: "summer.smith@gmail.com",
+    status: "active"
+  },
+  {
+    name: "Bird Person",
+    email: "bird.person@birdworld.com",
+    status: "active"
+  }
+]
+
+subscribers.each do |subscriber|
+  Subscriber.create!(email: subscriber[:email], name: subscriber[:name], status: subscriber[:status])
+end
+
+27.times do |n|
+  email = Faker::Internet.unique.email
+  name = Faker::Name.name
+  status = ['active', 'inactive'].sample
+  Subscriber.create!(email: email, name: name, status: status)
+end
