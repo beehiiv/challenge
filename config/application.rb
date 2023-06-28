96c54e321f28f0ead5e55972aa58a47ada36b602
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-06-28 09:34:57 -0600

    Move login credentials to env variables.

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,15 @@ module Challenge
     config.load_defaults 6.1
 
     # Configuration for the application, engines, and railties goes here.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'env.yml')
+      if File.exists?(env_file)
+        YAML.load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
+
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
