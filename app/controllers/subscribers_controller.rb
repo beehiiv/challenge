ed85f153d09a60c9cc0802afbf1ff9c821b3c195
Sharon Craddock <sharoncraddock.tech@gmail.com>
2023-06-28 11:20:59 -0600

    Subscribers now returning from the database.

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -6,44 +6,7 @@ class SubscribersController < ApplicationController
   ##
   # GET /api/subscribers
   def index
-    subscribers = [
-      {
-        id: 1,
-        name: "Rick Sanchez",
-        email: "rickc137@citadel.com",
-        status: "active"
-      },
-      {
-        id: 2,
-        name: "Morty Smith",
-        email: "morty.smith@gmail.com",
-        status: "inactive"
-      },
-      {
-        id: 3,
-        name: "Jerry Smith",
-        email: "jerry.smith@aol.com",
-        status: "active"
-      },
-      {
-        id: 4,
-        name: "Beth Smith",
-        email: "beth.smith@gmail.com",
-        status: "active"
-      },
-      {
-        id: 5,
-        name: "Summer Smith",
-        email: "summer.smith@gmail.com",
-        status: "active"
-      },
-      {
-        id: 6,
-        name: "Bird Person",
-        email: "bird.person@birdworld.com",
-        status: "active"
-      }
-    ]
+    subscribers = Subscriber.all
 
     total_records = subscribers.count
     limited_subscribers = subscribers[offset..limit]
